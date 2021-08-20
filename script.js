a18b3278da57769eeaef3092c7b12466ca3b0c67
Volkmar Petschnig <puzzled.promise@pm.me>
2021-08-20 08:29:30 +0200

    Add ability to open/close answer

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+let icons = document.querySelectorAll('.arrow');
+
+for (let i = 0; i < icons.length; i++) {
+  icons[i].addEventListener('click', clickArrow);
+}
+
+function clickArrow(e) {
+  icon = e.target;
+  faqEntry = icon.parentElement;
+  question = faqEntry.getElementsByClassName('question')[0];
+  answer = faqEntry.getElementsByClassName('answer')[0];
+
+  icon.classList.toggle('selected');
+  question.classList.toggle('selected');
+  answer.classList.toggle('selected');
+}
